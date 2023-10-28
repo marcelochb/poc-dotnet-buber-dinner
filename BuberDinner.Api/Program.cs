b2172427a91200781d9c6d839aee96361c2d1bba
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-10-28 12:05:30 -0300

    feat: AuthenticationService

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,9 @@
+using BuberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 }
 
 
