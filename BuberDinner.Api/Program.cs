c8105c8b4ed526796dd80063c58ea49be6bddc94
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-12-18 01:57:40 -0300

    feat: Custom Problem details factory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,7 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -7,11 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
